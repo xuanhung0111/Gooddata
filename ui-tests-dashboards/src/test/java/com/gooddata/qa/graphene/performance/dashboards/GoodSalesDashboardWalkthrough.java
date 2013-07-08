3ce982065903a98e465b6117864e3556a63179cb
Michal Vanco <michal.vanco@gooddata.com>
2013-07-08 13:03:05 +0200

    TEST: QA-2103 Added advanced perf output with metadata  + added duplicated scripts for updating perf data in GD from fruit project

--- ui-tests-dashboards/src/test/java/com/gooddata/qa/graphene/performance/dashboards/GoodSalesDashboardWalkthrough.java
+++ ui-tests-dashboards/src/test/java/com/gooddata/qa/graphene/performance/dashboards/GoodSalesDashboardWalkthrough.java
@@ -2,10 +2,15 @@ package com.gooddata.qa.graphene.performance.dashboards;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.jboss.arquillian.graphene.Graphene;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -44,7 +49,7 @@ public class GoodSalesDashboardWalkthrough extends AbstractTest {
 	}
 	
 	@Test(dependsOnMethods = { "createProject" })
-	public void dashboardsWalkthrough() throws InterruptedException {
+	public void dashboardsWalkthrough() throws InterruptedException, JSONException {
 		browser.get(getRootUrl() + PAGE_UI_PROJECT_PREFIX.replace("#s", "#_keepLogs=1&s") + projectId + "|projectDashboardPage");
 		waitForDashboardPageLoaded();
 		for (int i = 1; i <= 10; i++) {
@@ -64,7 +69,7 @@ public class GoodSalesDashboardWalkthrough extends AbstractTest {
 		projectPage.deteleProject();
 	}
 	
-	private void createPerfOutputFile(String csvContent) {
+	private void createPerfOutputFile(String csvContent) throws JSONException {
 		File mavenProjectBuildDirectory = new File(System.getProperty("maven.project.build.directory", "./target/"));
 		File outputFile = new File(mavenProjectBuildDirectory, "perf.csv");
 		try {
@@ -73,6 +78,45 @@ public class GoodSalesDashboardWalkthrough extends AbstractTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+		createPerfOutputFileWithMetadata(outputFile);
+	}
+	
+	private void createPerfOutputFileWithMetadata(File file) throws JSONException {
+		File outputFile = new File(file.getParentFile(), "perf_log.csv");
+		StringBuilder result = new StringBuilder();
+		String metaHeaders = "time,branch,build,builduri,gitcommit,browser";
+		String branch = System.getProperty("GD_PERFTIME_BRANCH_LABEL", "branch-not-set");
+		String buildNumber = System.getenv("BUILD_NUMBER");
+		String buildUrl = System.getenv("BUILD_URL");
+		String time = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+		String metaValues = time + "," + branch + "," + buildNumber + "," + buildUrl + "," + getClientVersion() + "," + System.getProperty("browser", "firefox") + ",";
+		result.append(metaHeaders + ",sessionid,start,duration,action" + "\n");
+		LineIterator it = null;
+		try {
+			it = FileUtils.lineIterator(file);
+			while (it.hasNext()) {
+				String line = it.nextLine();
+				result.append(metaValues).append(line).append("\n");
+			}
+			FileUtils.writeStringToFile(outputFile, result.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			it.close();
+		}
+	}
+	
+	private String getClientVersion() throws JSONException {
+		browser.get(getRootUrl() + "/gdc/releaseInfo");
+		JSONObject json = loadJSON();
+		JSONArray components = json.getJSONObject("release").getJSONArray("components");
+		for (int i = 0; i < components.length(); i++) {
+			JSONObject component = components.getJSONObject(i);
+			if (component.getJSONObject("component").getString("name").equals("client")) {
+				return component.getJSONObject("component").getString("release");
+			}
+		}
+		return "client-version-not-found";
 	}
 	
 }
