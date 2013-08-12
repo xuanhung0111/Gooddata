2e2f72e6d50713f5c0b144fe026c294fad4faf4d
Michal Vanco <michal.vanco@gooddata.com>
2013-08-12 14:58:21 +0200

    TEST: Fixed failing ui core test (simple project - delete project when no other projects are present)

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/manage/ProjectAndUsersPage.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/manage/ProjectAndUsersPage.java
@@ -1,6 +1,5 @@
 package com.gooddata.qa.graphene.fragments.manage;
 
-import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.enricher.findby.FindBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -15,7 +14,7 @@ public class ProjectAndUsersPage extends AbstractFragment {
 	@FindBy(xpath="//form/div/span/button[text()='Delete']")
 	private WebElement deleteProjectDialogButton;
 	
-	private static final By BY_PROJECTS_LIST = By.id("myProjects");
+	private static final By BY_PROJECTS_LIST = By.className("userProjects");
 	
 	public void deteleProject() {
 		waitForElementVisible(deleteProjectButton);
