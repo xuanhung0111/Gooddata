6afbcdd5fc0623bfd83860da2ccd824a88d19dac
Michal Vanco <michal.vanco@gooddata.com>
2013-12-10 22:10:18 +0100

    TEST: QA-3031 Refactoring of SimpleProjectTest  - abstract class SimpleProjectAbstractTest created with shared methods  - simplified GeoCharts/SimpleProject/SimpleWorkshop tests

--- ui-tests-core/src/test/java/com/gooddata/qa/graphene/project/SimpleWorkshopTest.java
+++ ui-tests-core/src/test/java/com/gooddata/qa/graphene/project/SimpleWorkshopTest.java
@@ -4,53 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jboss.arquillian.graphene.Graphene;
-import org.json.JSONException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.gooddata.qa.graphene.AbstractTest;
 import com.gooddata.qa.graphene.enums.ReportTypes;
 import com.gooddata.qa.graphene.fragments.dashboards.DashboardReportOneNumber;
 import com.gooddata.qa.utils.graphene.Screenshots;
 
-@Test(groups = { "projectSimpleWS" }, description = "Tests for workshop simple project in GD platform")
-public class SimpleWorkshopTest extends AbstractTest {
+@Test(groups = { "projectSimpleWS" }, description = "Tests for simple workshop test in GD platform")
+public class SimpleWorkshopTest extends SimpleProjectAbstractTest {
 	
 	private String csvFilePath;
 	
 	@BeforeClass
-	public void initStartPage() {
-		startPage = "projects.html";
-	
+	public void initProperties() {
 		csvFilePath = loadProperty("csvFilePath");
+		projectTitle = "simple-project-ws";
 	}
 	
-	@Test(groups = { "projectSimpleWSInit" } )
-	public void init() throws JSONException {
-		// sign in with demo user
-		signInAtUI(user, password);
-	}
-	
-	@Test(dependsOnGroups = { "projectSimpleWSInit" })
-	public void createSimpleProjectWS() throws JSONException, InterruptedException {
-		openUrl(PAGE_GDC_PROJECTS);
-		waitForElementVisible(gpProject.getRoot());
-		projectId = gpProject.createProject("simple-project-ws", "", "", authorizationToken, 12);
-		Screenshots.takeScreenshot(browser, "simple-project-ws-created", this.getClass());
-	}
-	
-	@Test(dependsOnMethods = { "createSimpleProjectWS" }, groups = { "ws-charts" })
+	@Test(dependsOnMethods = { "createSimpleProject" }, groups = { "simpleTests" })
 	public void uploadData() throws InterruptedException {
 		uploadSimpleCSV(csvFilePath + "/payroll.csv", "simple-ws");
 	}
 	
-	@Test(dependsOnMethods = { "uploadData" }, groups = { "ws-charts" })
+	@Test(dependsOnMethods = { "uploadData" }, groups = { "simpleTests" })
 	public void addNewTabs() throws InterruptedException {
 		addNewTabOnDashboard("Default dashboard", "workshop", "simple-ws");
 	}
 	
-	@Test(dependsOnMethods = { "uploadData" }, groups = "ws-charts")
+	@Test(dependsOnMethods = { "uploadData" }, groups = "simpleTests")
 	public void createBasicReport() throws InterruptedException {
 		initReportsPage();
 		reportsPage.startCreateReport();
@@ -62,8 +45,7 @@ public class SimpleWorkshopTest extends AbstractTest {
 		Screenshots.takeScreenshot(browser, "simple-ws-headline-report", this.getClass());
 	}
 	
-	@Test(dependsOnMethods = { "createBasicReport" }, groups = { "ws-charts" })
-	
+	@Test(dependsOnMethods = { "createBasicReport" }, groups = { "simpleTests" })
 	public void addReportOnDashboardTab() throws InterruptedException {
 		initDashboardsPage();
 		dashboardsPage.getTabs().openTab(1);
@@ -75,7 +57,7 @@ public class SimpleWorkshopTest extends AbstractTest {
 		Screenshots.takeScreenshot(browser, "simple-ws-headline-report-dashboard", this.getClass());
 	}
 	
-	@Test(dependsOnMethods = { "addReportOnDashboardTab" }, groups = { "ws-charts" })
+	@Test(dependsOnMethods = { "addReportOnDashboardTab" }, groups = { "simpleTests" })
 	public void verifyHeadlineReport() {
 		initDashboardsPage();
 		//Assert.assertEquals(1, dashboardsPage.getContent().getNumberOfReports(), "Report is not available on dashboard");
@@ -84,10 +66,5 @@ public class SimpleWorkshopTest extends AbstractTest {
 		Assert.assertEquals(report.getValue(), "7,252,542.63", "Invalid value in headline report");
 		Assert.assertEquals(report.getDescription(), "Sum of Amount", "Invalid description in headline report");
 		successfulTest = true;
-	}
-	
-	@Test(dependsOnGroups = { "ws-charts" }, alwaysRun = true)
-	public void deleteSimpleProject() {
-		deleteProjectByDeleteMode(successfulTest);
 	}
 }
