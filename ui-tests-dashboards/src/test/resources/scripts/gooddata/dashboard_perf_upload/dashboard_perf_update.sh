42d502b79760e8e50370a40162c883676bb24b3d
Michal Vanco <michal.vanco@gooddata.com>
2014-01-19 22:42:27 +0100

    CONFIG: QA-3245 Updated uploading tool to moved project (na1)

--- ui-tests-dashboards/src/test/resources/scripts/gooddata/dashboard_perf_upload/dashboard_perf_update.sh
+++ ui-tests-dashboards/src/test/resources/scripts/gooddata/dashboard_perf_upload/dashboard_perf_update.sh
@@ -1,1 +1,1 @@
-$CL_TOOL_CMD -u david.vrzalik+cpt@gooddata.com -p wlcmttm2 -a INT000000GDENQA dashboard_perf_update.gdc
+$CL_TOOL_CMD -u michal.vanco+cli@gooddata.com -p 'jqWA5SA|Jh#o9Hj' -h na1.secure.gooddata.com -a INT000000GDENQA dashboard_perf_update.gdc
