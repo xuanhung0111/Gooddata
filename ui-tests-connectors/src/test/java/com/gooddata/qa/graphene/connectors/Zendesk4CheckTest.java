af5f6587fb0b82f8585d191e9eee1b3a9569fc83
Michal Vanco <michal.vanco@gooddata.com>
2014-03-26 00:58:08 +0100

    FEATURE: QA-3694 Updated Zendesk4 graphene tests for ETL M1  * apiUrl is set with REST client (since grey page for settings is not ready yet)

--- ui-tests-connectors/src/test/java/com/gooddata/qa/graphene/connectors/Zendesk4CheckTest.java
+++ ui-tests-connectors/src/test/java/com/gooddata/qa/graphene/connectors/Zendesk4CheckTest.java
@@ -4,28 +4,63 @@
 package com.gooddata.qa.graphene.connectors;
 
 import com.gooddata.qa.graphene.enums.Connectors;
+import com.gooddata.qa.utils.http.RestApiClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.jboss.arquillian.graphene.Graphene;
 import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 @Test(groups = {"connectors", "zendesk4"}, description = "Checklist tests for Zendesk4 REST API")
 public class Zendesk4CheckTest extends AbstractConnectorsCheckTest {
 
+    private String zendeskUploadUser;
+    private String zendeskUploadUserPassword;
+    private String zendeskApiUrl;
+
     @BeforeClass
     public void loadRequiredProperties() {
+        zendeskApiUrl = loadProperty("connectors.zendesk.apiUrl");
+        zendeskUploadUser = loadProperty("connectors.zendesk.uploadUser");
+        zendeskUploadUserPassword = loadProperty("connectors.zendesk.uploadUserPassword");
+
         connectorType = Connectors.ZENDESK4;
+        expectedDashboardTabs = new String[]{
+                "Public comments", "Comments by hour", "Group reassignments", "Comments by group"
+        };
     }
 
-    @Override
-    @Test(groups = {"connectorWalkthrough"}, dependsOnMethods = {"testConnectorProcessesResource"})
-    public void verifyProjectDashboards() throws InterruptedException {
-        // TODO temporary workaround for missing Zendesk4 dashboards
-        successfulTest = true;
+    @Test(groups = {"connectorWalkthrough", "connectorIntegration"}, dependsOnMethods = { "testConnectorIntegrationResource" })
+    public void testZendesk4IntegrationConfiguration() throws InterruptedException, JSONException {
+        RestApiClient restApiClient = new RestApiClient(host, zendeskUploadUser, zendeskUploadUserPassword);
+        openUrl(getIntegrationUri());
+        // go to page with integration settings (note that settings URL is different from Z3
+        Graphene.guardHttp(waitForElementVisible(BY_GP_SETTINGS_LINK)).click();
+        JSONObject json = loadJSON();
+        assertEquals(json.getJSONObject("settings").getString("apiUrl"), "null", "Zendesk4 API URL was not set to expected value");
+        String settingsUrl = browser.getCurrentUrl();
+
+        // zendesk4 specific configuration of API Url (with specific upload user)
+        signInAtGreyPages(zendeskUploadUser, zendeskUploadUserPassword);
+        browser.get(settingsUrl);
+        HttpRequestBase putRequest = restApiClient.newPutMethod(settingsUrl, "{\"settings\": {\"apiUrl\": \"" + zendeskApiUrl + "\"}}");
+        HttpResponse putResponse = restApiClient.execute(putRequest);
+        Assert.assertTrue(putResponse.getStatusLine().getStatusCode() == 204, "Settings wasn't updated to required value");
+        browser.get(settingsUrl);
+        json = loadJSON();
+        assertEquals(json.getJSONObject("settings").getString("apiUrl"), zendeskApiUrl, "Zendesk4 API URL was not set to expected value");
     }
 
-    @Test(groups = {"connectorBasicREST", "connectorIntegration"}, dependsOnGroups = {"connectorInit"})
-    public void testZendesk4ScheduleProcess() throws JSONException, InterruptedException {
-        // schedule process
+    @Test(groups = {"connectorWalkthrough", "connectorIntegration"}, dependsOnMethods = { "testZendesk4IntegrationConfiguration" })
+    public void testZendesk4Integration() throws InterruptedException, JSONException {
+        // sign in back with demo user
+        validSignInWithDemoUser(true);
+        // process schedule
         scheduleIntegrationProcess(integrationProcessCheckLimit);
     }
 
