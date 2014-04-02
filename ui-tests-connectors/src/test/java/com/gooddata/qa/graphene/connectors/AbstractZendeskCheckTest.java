828392df6d348559914304bc74da460a1ce040ce
Matej Kupciha <matej.kupciha@gooddata.com>
2014-04-02 08:46:30 +0200

    TEST: ATP-1670 zendesk4: Set apiUrl using new grey pages form

---
+++ ui-tests-connectors/src/test/java/com/gooddata/qa/graphene/connectors/AbstractZendeskCheckTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2007-2014, GoodData(R) Corporation. All rights reserved.
+ */
+package com.gooddata.qa.graphene.connectors;
+
+import org.jboss.arquillian.graphene.Graphene;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public abstract class AbstractZendeskCheckTest extends AbstractConnectorsCheckTest {
+
+    protected static final By BY_INPUT_API_URL = By.name("apiUrl");
+
+    protected String zendeskUploadUser;
+    protected String zendeskUploadUserPassword;
+    protected String zendeskApiUrl;
+
+    public void loadRequiredProperties() {
+        zendeskApiUrl = loadProperty("connectors.zendesk.apiUrl");
+        zendeskUploadUser = loadProperty("connectors.zendesk.uploadUser");
+        zendeskUploadUserPassword = loadProperty("connectors.zendesk.uploadUserPassword");
+    }
+
+    @Test(groups = {"connectorWalkthrough", "connectorIntegration"}, dependsOnMethods = {"testConnectorIntegrationResource"})
+    public void testZendeskIntegrationConfiguration() throws InterruptedException, JSONException {
+        openUrl(getIntegrationUri());
+        // go to page with integration settings (differs for Zendesk3/4)
+        String settingsUrl = openZendeskSettingsUrl();
+        JSONObject json = loadJSON();
+        assertEquals(json.getJSONObject("settings").getString("apiUrl"), "null",
+                String.format("%s API URL was not set to expected value", connectorType.getName()));
+
+        // zendesk specific configuration of API Url (with specific upload user)
+        signInAtGreyPages(zendeskUploadUser, zendeskUploadUserPassword);
+        browser.get(settingsUrl);
+        waitForElementPresent(BY_INPUT_API_URL).sendKeys(zendeskApiUrl);
+        Graphene.guardHttp(waitForElementPresent(BY_GP_BUTTON_SUBMIT)).click();
+        json = loadJSON();
+        assertEquals(json.getJSONObject("settings").getString("apiUrl"), zendeskApiUrl,
+                String.format("%s API URL was not set to expected value", connectorType.getName()));
+    }
+
+    @Test(groups = {"connectorWalkthrough", "connectorIntegration"}, dependsOnMethods = {"testZendeskIntegrationConfiguration"})
+    public void testZendeskIntegration() throws InterruptedException, JSONException {
+        // sign in back with demo user
+        validSignInWithDemoUser(true);
+        // process schedule
+        scheduleIntegrationProcess(integrationProcessCheckLimit);
+    }
+
+    public abstract String openZendeskSettingsUrl();
+}
--- ui-tests-connectors/src/test/java/com/gooddata/qa/graphene/connectors/Zendesk3CheckTest.java
+++ ui-tests-connectors/src/test/java/com/gooddata/qa/graphene/connectors/Zendesk3CheckTest.java
@@ -1,35 +1,20 @@
 package com.gooddata.qa.graphene.connectors;
 
-import org.jboss.arquillian.graphene.Graphene;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.openqa.selenium.By;
+import com.gooddata.qa.graphene.enums.Connectors;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.gooddata.qa.graphene.enums.Connectors;
-
 import java.util.HashMap;
 
-import static org.testng.Assert.*;
+@Test(groups = {"connectors", "zendesk3"}, description = "Checklist tests for Zendesk3 connector in GD platform")
+public class Zendesk3CheckTest extends AbstractZendeskCheckTest {
 
-@Test(groups = { "connectors", "zendesk3" }, description = "Checklist tests for Zendesk3 connector in GD platform")
-public class Zendesk3CheckTest extends AbstractConnectorsCheckTest {
-	
-	private String zendeskUploadUser;
-	private String zendeskUploadUserPassword;
-	private String zendeskApiUrl;
-	
-	private static final By BY_INPUT_API_URL = By.name("apiUrl");
-	
-	@BeforeClass
-	public void loadRequiredProperties() {
-		zendeskApiUrl = loadProperty("connectors.zendesk.apiUrl");
-		zendeskUploadUser = loadProperty("connectors.zendesk.uploadUser");
-		zendeskUploadUserPassword = loadProperty("connectors.zendesk.uploadUserPassword");
-		
-		connectorType = Connectors.ZENDESK3;
-		expectedDashboardsAndTabs = new HashMap<String, String[]>();
+    @BeforeClass
+    public void loadRequiredProperties() {
+        super.loadRequiredProperties();
+
+        connectorType = Connectors.ZENDESK3;
+        expectedDashboardsAndTabs = new HashMap<String, String[]>();
         expectedDashboardsAndTabs.put("Advanced Metrics", new String[]{
                 "Overview", "Ticket Creation", "Ticket Distribution", "Performance", "Backlog", "Open Issues", "Customer Satisfaction"
         });
@@ -39,28 +24,10 @@ public class Zendesk3CheckTest extends AbstractConnectorsCheckTest {
         expectedDashboardsAndTabs.put("Release 3 New Dashboards", new String[]{
                 "Overview w/ Business Hours", "Staffing and Volume Heat Maps"
         });
-	}
-	
-	@Test(groups = {"connectorWalkthrough", "connectorIntegration"}, dependsOnMethods = { "testConnectorIntegrationResource" })
-	public void testZendesk3IntegrationConfiguration() throws InterruptedException, JSONException {
-		openUrl(getIntegrationUri());
-		// go to page with integration settings
-		String settingsUrl = gotoIntegrationSettings();
-		
-		// zendesk3 specific configuration of API Url (with specific upload user)
-		signInAtGreyPages(zendeskUploadUser, zendeskUploadUserPassword);
-		browser.get(settingsUrl);
-		waitForElementPresent(BY_INPUT_API_URL).sendKeys(zendeskApiUrl);
-		Graphene.guardHttp(waitForElementPresent(BY_GP_BUTTON_SUBMIT)).click();
-		JSONObject json = loadJSON();
-		assertEquals(json.getJSONObject("settings").getString("apiUrl"), zendeskApiUrl, "Zendesk3 API URL was not set to expected value");
-	}
-	
-	@Test(groups = {"connectorWalkthrough", "connectorIntegration"}, dependsOnMethods = { "testZendesk3IntegrationConfiguration" })
-	public void testZendesk3Integration() throws InterruptedException, JSONException {
-		// sign in back with demo user
-		validSignInWithDemoUser(true);
-		// process schedule
-		scheduleIntegrationProcess(integrationProcessCheckLimit);
-	}
+    }
+
+    @Override
+    public String openZendeskSettingsUrl() {
+        return gotoIntegrationSettings();
+    }
 }
