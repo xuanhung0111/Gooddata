31b5a37103e2f1c0eee522d4244663c16ff51fb1
Peter Lesko <peter.lesko@gooddata.com>
2014-05-28 08:45:34 +0200

    TEST: QA-4072 SLIManifest comparation test

---
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/greypages/md/ldm/singleloadinterface/SingleLoadInterfaceFragment.java
@@ -0,0 +1,27 @@
+package com.gooddata.qa.graphene.fragments.greypages.md.ldm.singleloadinterface;
+
+import com.gooddata.qa.graphene.fragments.greypages.AbstractGreyPagesFragment;
+import org.jboss.arquillian.graphene.Graphene;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SingleLoadInterfaceFragment extends AbstractGreyPagesFragment {
+
+    @FindBy
+    private WebElement dataset;
+
+    @FindBy
+    private WebElement submit;
+
+    public JSONObject postDataset(String datasetName) throws JSONException, InterruptedException {
+        waitForElementVisible(this.dataset).sendKeys(datasetName);
+        Graphene.guardHttp(submit).click();
+        waitForElementVisible(BY_GP_LINK);
+        String string = loadJSON().getJSONObject("sliLinks").getJSONObject("manifest").getString("uri");
+        Graphene.guardHttp(browser.findElement(By.linkText(string))).click();
+        return loadJSON();
+    }
+}
--- ui-tests-core/src/test/java/com/gooddata/qa/graphene/project/SimpleProjectEtlTest.java
+++ ui-tests-core/src/test/java/com/gooddata/qa/graphene/project/SimpleProjectEtlTest.java
@@ -2,17 +2,22 @@ package com.gooddata.qa.graphene.project;
 
 import com.gooddata.qa.graphene.AbstractProjectTest;
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.testng.Assert;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.zip.CRC32;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 @Test(groups = {"projectSimple"}, description = "Tests for basic ETL functionality in GD platform")
 public class SimpleProjectEtlTest extends AbstractProjectTest {
@@ -20,6 +25,27 @@ public class SimpleProjectEtlTest extends AbstractProjectTest {
     protected int statusPollingCheckIterations = 60; // (60*5s)
     private static final boolean exportUsers = true;
     private static final boolean exportData = true;
+
+    private class SLIManifestPart {
+
+        private final String columnName;
+        private final List<String> populateFields;
+
+        public SLIManifestPart(String columnName, List<String> populateFields) {
+            this.populateFields = populateFields;
+            this.columnName = columnName;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 1;
+            result += this.columnName.hashCode();
+            for (String s : this.populateFields) {
+                result += s.hashCode();
+            }
+            return result;
+        }
+    }
 
     @Test(dependsOnMethods = {"createProject"}, groups = {"tests"})
     public void loadProject() throws JSONException, URISyntaxException, IOException, InterruptedException {
@@ -42,6 +68,39 @@ public class SimpleProjectEtlTest extends AbstractProjectTest {
     }
 
     @Test(dependsOnMethods = {"loadProject"}, groups = {"tests"})
+    public void sliManifestsCompare() throws JSONException, IOException, InterruptedException {
+        HashSet<Object>  sliParts = new HashSet<Object>();
+
+        /** parse uploaded manifest first **/
+        URL uploadInfoResource = this.getClass().getResource("/etl/upload_info.json");
+
+        JSONObject dataSetSLIManifest = new JSONObject(IOUtils.toString(uploadInfoResource)).getJSONObject("dataSetSLIManifest");
+
+        String uploadInfoFile = dataSetSLIManifest.getString("file");
+        String uploadInfoDataset = dataSetSLIManifest.getString("dataSet");
+
+        JSONArray jsonPartsArray = dataSetSLIManifest.getJSONArray("parts");
+        for (int i = 0; i < jsonPartsArray.length(); i++) {
+            String columnName = jsonPartsArray.getJSONObject(i).getString("columnName");
+            ArrayList<String> populateFields = parsePopulatesFields(jsonPartsArray.getJSONObject(i).getJSONArray("populates"));
+            sliParts.add(new SLIManifestPart(columnName, populateFields).hashCode());
+        }
+
+        /** check generated sli manifest **/
+        dataSetSLIManifest = fetchSLIManifest(uploadInfoDataset).getJSONObject("dataSetSLIManifest");
+        jsonPartsArray = dataSetSLIManifest.getJSONArray("parts");
+        assertEquals(jsonPartsArray.length(), sliParts.size(),"SLIManifest parts count doesn't match");
+        assertEquals(dataSetSLIManifest.getString("file"), "dataset.ds_"+uploadInfoFile, "SLIManifest file names doesn't match");
+        assertEquals(dataSetSLIManifest.getString("dataSet"), uploadInfoDataset, "SLIManifest dataset names doesn't match");
+
+        for (int i = 0; i < jsonPartsArray.length(); i++) {
+            String columnName = jsonPartsArray.getJSONObject(i).getString("columnName");
+            ArrayList<String> populateFields = parsePopulatesFields(jsonPartsArray.getJSONObject(i).getJSONArray("populates"));
+            assertTrue(sliParts.contains(new SLIManifestPart(columnName, populateFields).hashCode()));
+        }
+    }
+
+    @Test(dependsOnMethods = {"loadProject"}, groups = {"tests"})
     public void exportImportProject() throws JSONException, InterruptedException {
         String exportToken = exportProject(exportUsers, exportData, statusPollingCheckIterations);
         String parentProjectId = projectId;
@@ -54,6 +113,14 @@ public class SimpleProjectEtlTest extends AbstractProjectTest {
 
         projectId = parentProjectId;
         successfulTest = true;
+    }
+
+    private ArrayList<String> parsePopulatesFields(JSONArray populates) throws JSONException {
+        ArrayList<String> populateFields = new ArrayList<String>();
+        for (int j = 0; j < populates.length(); j++) {
+            populateFields.add(populates.getString(j));
+        }
+        return populateFields;
     }
 
     public static long getCRC(InputStream inputStreamn) throws IOException {
