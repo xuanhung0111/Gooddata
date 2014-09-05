2d7ca7633528dadcc6dda4b7a7d3bfba0706e414
Jan Hruban <jan.hruban@gooddata.com>
2014-09-05 13:07:12 +0200

    TRIVIAL: fix indentation

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/manage/AttributePage.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/manage/AttributePage.java
@@ -18,35 +18,35 @@ public class AttributePage extends AbstractFragment {
     protected AttributeDetailPage attributeDetailPage;
 
     public void configureAttributeLabel(String attributeName,
-	    AttributeLabelTypes attributeLabel) throws InterruptedException {
-	initAttribute(attributeName);
-	assertEquals(attributeDetailPage.getAttributeName(), attributeName,
-		"Invalid attribute name on detail page");
-	attributeDetailPage.selectLabelType(attributeLabel.getlabel());
-	Thread.sleep(2000);
-	assertEquals(attributeDetailPage.getAttributeLabelType(),
-		attributeLabel.getlabel(), "Label type not set properly");
+        AttributeLabelTypes attributeLabel) throws InterruptedException {
+        initAttribute(attributeName);
+        assertEquals(attributeDetailPage.getAttributeName(), attributeName,
+        "Invalid attribute name on detail page");
+        attributeDetailPage.selectLabelType(attributeLabel.getlabel());
+        Thread.sleep(2000);
+        assertEquals(attributeDetailPage.getAttributeLabelType(),
+        attributeLabel.getlabel(), "Label type not set properly");
     }
 
     public void verifyHyperLink(String attributeName) {
-	initAttribute(attributeName);
-	assertTrue(attributeDetailPage.isHyperLink(),
-		"Attribute is NOT hyperlink, probably failed in label configuration!");
+        initAttribute(attributeName);
+        assertTrue(attributeDetailPage.isHyperLink(),
+        "Attribute is NOT hyperlink, probably failed in label configuration!");
     }
 
     public void configureDrillToExternalPage(String attributeName) {
-	initAttribute(attributeName);
-	attributeDetailPage.setDrillToExternalPage();
+        initAttribute(attributeName);
+        attributeDetailPage.setDrillToExternalPage();
     }
 
     public void initAttribute(String attributeName) {
-	waitForElementVisible(attributesTable.getRoot());
-	waitForDataPageLoaded(browser);
-	attributesTable.selectObject(attributeName);
-	waitForObjectPageLoaded(browser);
-	String variableDetailsWindowHandle = browser.getWindowHandle();
-	browser.switchTo().window(variableDetailsWindowHandle);
-	waitForElementVisible(attributeDetailPage.getRoot());
+        waitForElementVisible(attributesTable.getRoot());
+        waitForDataPageLoaded(browser);
+        attributesTable.selectObject(attributeName);
+        waitForObjectPageLoaded(browser);
+        String variableDetailsWindowHandle = browser.getWindowHandle();
+        browser.switchTo().window(variableDetailsWindowHandle);
+        waitForElementVisible(attributeDetailPage.getRoot());
     }
 
 }
