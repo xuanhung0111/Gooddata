12765ba923ecaf03f2610216a9fd09cd8e667a13
Michal Vanco <michal.vanco@gooddata.com>
2015-02-02 16:06:54 +0700

    TRIVIAL: Added logging of URL at AttributeLabelsTest

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/reports/ReportWithImage.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/reports/ReportWithImage.java
@@ -16,27 +16,28 @@ public class ReportWithImage extends AbstractReport {
     private List<WebElement> images;
 
     public void verifyImageOnReport() {
-	String noImageURL = "/images/2011/il-no-image.png";
-	for (WebElement element : images) {
-	    assertFalse(waitForElementVisible(element).getAttribute("src")
-		    .endsWith(noImageURL), "Image is NOT rendered in report!");
-	}
+        String noImageURL = "/images/2011/il-no-image.png";
+        for (WebElement element : images) {
+            assertFalse(waitForElementVisible(element).getAttribute("src")
+                    .endsWith(noImageURL), "Image is NOT rendered in report!");
+        }
     }
 
     public void verifyIfImageSFDCOnReport() {
-	ArrayList<String> urls = getImagesLinks();
-	for (int i = 0; i < urls.size(); i++) {
-	    browser.get(urls.get(i));
-	    assertFalse((browser.getCurrentUrl().endsWith("not+found.") || browser.getCurrentUrl().contains("errorCode")),
-		    "SFDC image is NOT rendered in report!");
-	}
+        ArrayList<String> urls = getImagesLinks();
+        for (String url : urls) {
+            System.out.println("Going to check image on url: " + url);
+            browser.get(url);
+            assertFalse((browser.getCurrentUrl().endsWith("not+found.") || browser.getCurrentUrl().contains("errorCode")),
+                    "SFDC image is NOT rendered in report!");
+        }
     }
 
     private ArrayList<String> getImagesLinks() {
-	ArrayList<String> imageLinks = new ArrayList<String>();
-	for (WebElement element : images) {
-	    imageLinks.add(waitForElementVisible(element).getAttribute("src"));
-	}
-	return imageLinks;
+        ArrayList<String> imageLinks = new ArrayList<String>();
+        for (WebElement element : images) {
+            imageLinks.add(waitForElementVisible(element).getAttribute("src"));
+        }
+        return imageLinks;
     }
 }
