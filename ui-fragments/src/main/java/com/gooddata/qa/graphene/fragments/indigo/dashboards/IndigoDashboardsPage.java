7d35a9bddd4108277716c3021c0d4ea8afabd3be
David Vavrousek <david.vavrousek@gooddata.com>
2015-06-12 13:54:42 +0200

    CODE: CL-7509 dashboard test kip headline change - 4th comments (+5 squashed commits) Squashed commits: [59cffaf] CODE: CL-7509 dashboard test kip headline change - 3rd comments [4efad34] CODE: CL-7509 dashboard test kip headline change - 2nd comments [56f0827] CODE: CL-7509 dashboard test kip headline change - comments [5ca6370] CODE: CL-7509 dashboard test kip headline change [ad57bcd] CODE: CL-7509 dashboard cancel edit mode test

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/indigo/dashboards/IndigoDashboardsPage.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/indigo/dashboards/IndigoDashboardsPage.java
@@ -1,11 +1,65 @@
 package com.gooddata.qa.graphene.fragments.indigo.dashboards;
 
 import com.gooddata.qa.graphene.fragments.AbstractFragment;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import static com.gooddata.qa.graphene.common.CheckUtils.waitForElementVisible;
 
 import java.util.List;
+
+import static com.gooddata.qa.graphene.common.CheckUtils.waitForElementNotPresent;
+import static com.gooddata.qa.graphene.common.CheckUtils.waitForElementPresent;
 
 public class IndigoDashboardsPage extends AbstractFragment {
     @FindBy(className = Kpi.MAIN_CLASS)
     protected List<Kpi> kpis;
+
+    @FindBy(className = "s-edit_button")
+    protected WebElement editButton;
+
+    @FindBy(className = "s-cancel_button")
+    protected WebElement cancelButton;
+
+    @FindBy(className = "s-metric_select")
+    protected WebElement metricSelect;
+
+    public IndigoDashboardsPage switchToEditMode() {
+        waitForElementVisible(editButton).click();
+        waitForElementVisible(this.cancelButton);
+
+        return this;
+    }
+
+    public IndigoDashboardsPage cancelEditMode() {
+        waitForElementVisible(cancelButton).click();
+        waitForElementVisible(this.editButton);
+
+        return this;
+    }
+
+    public Kpi selectKpi(int index) {
+        Kpi tempKpi = getKpiByIndex(index);
+        tempKpi.getRoot().click();
+        waitForElementPresent(metricSelect);
+        return tempKpi;
+    }
+
+    public Kpi getKpiByIndex(int index) {
+        return kpis.get(index);
+    }
+
+    public IndigoDashboardsPage selectMetricByIndex(int index) {
+        Select dropDown = new Select(waitForElementVisible(this.metricSelect));
+        dropDown.selectByIndex(index);
+        waitForKpiLoading();
+
+        return this;
+    }
+
+    public void waitForKpiLoading(){
+        waitForElementNotPresent(Kpi.IS_LOADING);
+    }
+
 }
--- ui-tests-core/src/test/java/com/gooddata/qa/graphene/indigo/dashboards/DashboardsGeneralTest.java
+++ ui-tests-core/src/test/java/com/gooddata/qa/graphene/indigo/dashboards/DashboardsGeneralTest.java
@@ -3,13 +3,57 @@ package com.gooddata.qa.graphene.indigo.dashboards;
 import com.gooddata.qa.graphene.GoodSalesAbstractTest;
 import com.gooddata.qa.graphene.fragments.indigo.dashboards.Kpi;
 import org.testng.annotations.Test;
-
-import static com.gooddata.qa.graphene.common.CheckUtils.*;
+import static org.testng.Assert.*;
 
 public class DashboardsGeneralTest extends GoodSalesAbstractTest {
-    @Test(dependsOnMethods = {"createProject"}, groups = {"loading"})
+
+    @Test(dependsOnMethods = {"createProject"})
     public void kpisLoadedCheck() {
         initIndigoDashboardsPage();
-        waitForElementNotPresent(Kpi.IS_LOADING);
     }
+
+    @Test(dependsOnMethods = {"createProject"})
+    public void checkEditModeCancel() {
+        Kpi selectedKpi;
+        String selectedKpiDataHeadline;
+        String selectedKpiDataValue;
+
+        initIndigoDashboardsPage();
+
+        indigoDashboardsPage.switchToEditMode();
+
+        selectedKpi = indigoDashboardsPage.selectKpi(0);
+        selectedKpiDataHeadline = selectedKpi.getHeadline();
+        selectedKpiDataValue = selectedKpi.getValue();
+
+        indigoDashboardsPage.selectMetricByIndex(0).cancelEditMode();
+
+        assertEquals(selectedKpi.getHeadline(), selectedKpiDataHeadline);
+        assertEquals(selectedKpi.getValue(), selectedKpiDataValue);
+    }
+
+    @Test(dependsOnMethods = {"createProject"})
+    public void checkKpiTitleChange() {
+        Kpi selectedKpi;
+        String selectedKpiDataHeadline;
+
+        initIndigoDashboardsPage();
+
+        indigoDashboardsPage.switchToEditMode();
+
+        selectedKpi = indigoDashboardsPage.selectKpi(0);
+        selectedKpiDataHeadline = selectedKpi.getHeadline();
+
+        selectedKpi.setHeadline("Test headline");
+
+        assertNotEquals(selectedKpi.getHeadline(), selectedKpiDataHeadline);
+
+        indigoDashboardsPage.selectMetricByIndex(0);
+        assertTrue(selectedKpi.getHeadline().equals("Test headline"));
+
+        indigoDashboardsPage.cancelEditMode();
+
+        assertEquals(selectedKpi.getHeadline(), selectedKpiDataHeadline);
+    }
+
 }
