5603fc9c1a517ae553eaffa30144b73338dd3cf0
Anh Nguyen Thi Quynh <ntqanh@lhv.vn>
2015-09-23 10:42:39 +0700

    TRIVIAL : CL-7889 add projectID into URI of Indigo dashboards

--- ui-core/src/main/java/com/gooddata/qa/graphene/AbstractUITest.java
+++ ui-core/src/main/java/com/gooddata/qa/graphene/AbstractUITest.java
@@ -91,7 +91,7 @@ public class AbstractUITest extends AbstractGreyPageTest {
     protected static final String PAGE_LOGIN = ACCOUNT_PAGE + "#/login";
     protected static final String DASHBOARD_PAGE_SUFFIX = "|projectDashboardPage";
     protected static final String PAGE_USER_MANAGEMENT = "users/#/users";
-    protected static final String PAGE_INDIGO_DASHBOARDS = "dashboards/";
+    protected static final String PAGE_INDIGO_DASHBOARDS = "dashboards/#/p/";
 
     protected static final String DISC_PROJECTS_PAGE_URL = "admin/disc/#/projects";
     protected static final String DISC_OVERVIEW_PAGE = "admin/disc/#/overview";
@@ -580,7 +580,7 @@ public class AbstractUITest extends AbstractGreyPageTest {
     }
 
     public IndigoDashboardsPage initIndigoDashboardsPage() {
-        openUrl(PAGE_INDIGO_DASHBOARDS);
+        openUrl(PAGE_INDIGO_DASHBOARDS + testParams.getProjectId());
         waitForFragmentVisible(indigoDashboardsPage);
 
         return indigoDashboardsPage
