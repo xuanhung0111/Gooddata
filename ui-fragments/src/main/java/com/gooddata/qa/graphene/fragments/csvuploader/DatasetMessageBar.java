409017665f279a39f59fcdace88fa937e196c370
Anh Nguyen Thi Quynh <ntqanh@lhv.vn>
2015-12-21 11:34:10 +0700

    TRIVIAL: remove completely unstable checking of progress bar

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/csvuploader/DatasetMessageBar.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/csvuploader/DatasetMessageBar.java
@@ -9,14 +9,9 @@ import com.gooddata.qa.graphene.fragments.AbstractFragment;
 
 public class DatasetMessageBar extends AbstractFragment{
     
-    private static final By BY_PROGRESS_MESSAGE_BAR = By.cssSelector(".gd-message.progress");
     private static final By BY_ERROR_MESSAGE_BAR = By.cssSelector(".gd-message.error");
     private static final By BY_SUCCESS_MESSAGE_BAR = By.cssSelector(".gd-message.success");
-    
-    public WebElement waitForProgressMessageBar() {
-        return waitForElementVisible(BY_PROGRESS_MESSAGE_BAR, getRoot());
-    }
-    
+
     public WebElement waitForErrorMessageBar() {
         return waitForElementVisible(BY_ERROR_MESSAGE_BAR, getRoot());
     }
--- ui-tests-msf/ui-tests-csv-uploader/src/test/java/com/gooddata/qa/graphene/csvuploader/AbstractCsvUploaderTest.java
+++ ui-tests-msf/ui-tests-csv-uploader/src/test/java/com/gooddata/qa/graphene/csvuploader/AbstractCsvUploaderTest.java
@@ -49,8 +49,6 @@ public class AbstractCsvUploaderTest extends AbstractMSFTest {
     protected static final String AD_REPORT_LINK = "https://%s/analyze/#/%s/reportId/edit?dataset=%s";
     protected static final String CSV_DATASET_DETAIL_PAGE_URI_TEMPLATE = DATA_UPLOAD_PAGE_URI_TEMPLATE + "/%s";
     protected static final String DATASET_LINK= "https://%s/data/#/projects/%s/datasets/%s";
-    private static final String ADDING_DATA_FROM_MESSAGE = "Adding data from \"%s\" ...";
-    protected static final String UPDATE_DATA_MESSAGE = "Updating data from \"%s\" ...";
     protected static final String SUCCESSFUL_DATA_MESSAGE = "Data has been loaded successfully to \"%s\". Start analyzing!";
     /**
      * Successful load contains information about number of rows and columns,
@@ -131,9 +129,6 @@ public class AbstractCsvUploaderTest extends AbstractMSFTest {
         takeScreenshot(browser, toScreenshotName(DATA_PREVIEW_PAGE, csvFile.getFileName()), getClass());
 
         dataPreviewPage.triggerIntegration();
-        
-        assertThat(csvDatasetMessageBar.waitForProgressMessageBar().getText(),
-                is(String.format(ADDING_DATA_FROM_MESSAGE, csvFile.getFileName())));
     }
 
     protected String getNewDataset(CsvFile csvFile) {
