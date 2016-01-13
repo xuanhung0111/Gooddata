5d9c050c6b64eea9d2e18c54f9eca96efa47f84c
phthong <phthong@lhv.vn>
2016-01-13 17:34:23 +0700

    FEATURE: QA-4215 Add redbar info to graphene log

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/utils/CheckUtils.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/utils/CheckUtils.java
@@ -1,13 +1,16 @@
 package com.gooddata.qa.graphene.utils;
 
-import static com.gooddata.qa.graphene.utils.WaitUtils.waitForElementVisible;
+import static com.gooddata.qa.graphene.utils.WaitUtils.*;
 import static java.lang.String.format;
 import static org.testng.Assert.fail;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 
 public final class CheckUtils {
+
+    private static final Logger log = Logger.getLogger(CheckUtils.class.getName());
 
     private static final String STATUS_BAR_SELECTOR = "div#status > div.box-%s div.leftContainer";
     private static final By BY_GREEN_BAR = By.cssSelector(format(STATUS_BAR_SELECTOR, "success"));
@@ -17,20 +20,33 @@ public final class CheckUtils {
     public static final By BY_RED_BAR_WARNING = By.cssSelector(format(STATUS_BAR_SELECTOR, "warning"));
     public static final By BY_BLUE_BAR = By.cssSelector(format(STATUS_BAR_SELECTOR, "info"));
     public static final By BY_DISMISS_BUTTON = By.cssSelector("div#status .s-btn-dismiss");
+    public static final By BY_ERROR_ID = By.cssSelector("div#status .c-error-errorId");
+    public static final By BY_ERROR_MESSAGE = By.cssSelector("div#status .c-error-message");
 
     private CheckUtils() {
     }
 
     public static void checkRedBar(SearchContext searchContext) {
-        if (searchContext.findElements(BY_RED_BAR).size() != 0) {
+        if (ElementUtils.isElementPresent(BY_RED_BAR,searchContext)) {
+            logRedBarMessageInfo(searchContext);
             fail("RED BAR APPEARED - " + searchContext.findElement(BY_RED_BAR).getText());
         }
-        if (searchContext.findElements(BY_RED_BAR_WARNING).size() != 0) {
+        if (ElementUtils.isElementPresent(BY_RED_BAR_WARNING,searchContext)) {
             fail("RED BAR APPEARED - " + searchContext.findElement(BY_RED_BAR_WARNING).getText());
         }
         //this kind of error appeared for the first time in geo chart
-        if (searchContext.findElements(BY_REPORT_ERROR).size() != 0 && searchContext.findElement(BY_REPORT_ERROR).isDisplayed()) {
+        if (ElementUtils.isElementPresent(BY_REPORT_ERROR,searchContext) && searchContext.findElement(BY_REPORT_ERROR).isDisplayed()) {
             fail("Report error APPEARED - " + searchContext.findElement(BY_REPORT_ERROR).getText());
+        }
+    }
+    
+    public static void logRedBarMessageInfo(SearchContext searchContext){
+    	if(ElementUtils.isElementPresent(BY_ERROR_ID,searchContext)) {
+    		log.info("ErrorId APPEARED - " + searchContext.findElement(BY_ERROR_ID).getAttribute("value"));
+        }
+        
+        if(ElementUtils.isElementPresent(BY_ERROR_MESSAGE,searchContext)) {
+        	log.info("STATUS BAR INFO MESSAGE APPEARED - " + searchContext.findElement(BY_ERROR_MESSAGE).getAttribute("value"));
         }
     }
 
