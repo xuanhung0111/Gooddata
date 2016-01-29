8482d1b5005a1795b3e316ef90f6db13edd3e3ca
Petr Benes <petr.benes@gooddata.com>
2016-01-29 15:43:22 +0100

    FEATURE: ONE-602 Show only public & my metrics

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/indigo/dashboards/DateDimensionSelect.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/indigo/dashboards/DateDimensionSelect.java
@@ -3,6 +3,12 @@ package com.gooddata.qa.graphene.fragments.indigo.dashboards;
 public class DateDimensionSelect extends ReactDropdown {
 
     @Override
+    public String getDropdownButtonCssSelector() {
+        // dimensions dropdown is ready immediately, so no .is-loaded flag
+        return "button";
+    }
+
+    @Override
     public String getDropdownCssSelector() {
         return ".overlay .dimensions-list";
     }
