852a08a98d1e2ac02ff783603e2002dc0e3bd254
vhtien <vhtien@lhv.vn>
2016-06-15 16:40:03 +0700

    TRIVIAL: Increase timeout for getting email from noreply@gooddata.com from 5 to 10 minutes

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/enums/GDEmails.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/enums/GDEmails.java
@@ -2,7 +2,7 @@ package com.gooddata.qa.graphene.enums;
 
 public enum GDEmails {
     NO_REPLY("no-reply@gooddata.com", 3),
-    NOREPLY("noreply@gooddata.com", 5),
+    NOREPLY("noreply@gooddata.com", 10),
     INVITATION("invitation@gooddata.com", 10),
     REGISTRATION("registration@gooddata.com", 10);
 
