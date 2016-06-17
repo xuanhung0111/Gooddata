2fb031971396a33c2eb0dc417854a431d7eb4d35
Peter Plochan <peter.plochan@gooddata.com>
2016-06-17 10:55:01 +0200

    BUGFIX: ATP-3889 check created Coupa instance on instances page
    This is workaround for unstable test when creating of new instance resulted in http response 503.

--- ui-tests-connectors/src/test/java/com/gooddata/qa/graphene/connectors/coupa/CoupaCheckTest.java
+++ ui-tests-connectors/src/test/java/com/gooddata/qa/graphene/connectors/coupa/CoupaCheckTest.java
@@ -2,14 +2,20 @@ package com.gooddata.qa.graphene.connectors.coupa;
 
 import static com.gooddata.qa.graphene.utils.WaitUtils.waitForElementPresent;
 import static com.gooddata.qa.graphene.utils.WaitUtils.waitForElementVisible;
+import static com.gooddata.qa.utils.graphene.Screenshots.takeScreenshot;
+import static org.apache.commons.lang.Validate.notEmpty;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import org.jboss.arquillian.graphene.Graphene;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,6 +24,8 @@ import com.gooddata.qa.graphene.connectors.AbstractConnectorsCheckTest;
 import com.gooddata.qa.graphene.enums.Connectors;
 import com.gooddata.qa.graphene.enums.user.UserRoles;
 import com.gooddata.qa.graphene.fragments.greypages.connectors.CoupaInstanceFragment;
+import com.gooddata.qa.utils.graphene.Screenshots;
+import com.google.common.base.Predicate;
 
 @Test(groups = {"connectors", "coupa"}, description = "Checklist tests for Coupa connector in GD platform")
 public class CoupaCheckTest extends AbstractConnectorsCheckTest {
@@ -82,6 +90,7 @@ public class CoupaCheckTest extends AbstractConnectorsCheckTest {
         // create coupa instance
         waitForElementPresent(coupaInstance.getRoot());
         coupaInstance.createCoupaInstance(Connectors.COUPA.getConnectorId(), coupaInstanceApiUrl, coupaInstanceApiKey);
+        verifyCoupaInstance(coupaInstanceApiUrl);
 
         // verify progress on Coupa dashboard
         openUrl(PAGE_UI_PROJECT_PREFIX + testParams.getProjectId());
@@ -111,5 +120,50 @@ public class CoupaCheckTest extends AbstractConnectorsCheckTest {
             dependsOnMethods = {"testCoupaIntegration"})
     public void testIncrementalSynchronization() throws JSONException {
         scheduleIntegrationProcess(integrationProcessCheckLimit);
+    }
+
+    /**
+     * Verifies if a created Coupa instance with the given api url exists on /instances endpoint.
+     * This method refreshes /instances page until the instance with api url is not present.
+     * If the refreshing exceeds given time period, the verification fails.
+     *
+     * @param apiUrl api url of the coupa instance to be verified
+     */
+    private void verifyCoupaInstance(final String apiUrl) {
+        openUrl(getIntegrationUri() + "/config/settings/instances");
+
+        final Predicate<WebDriver> predicate = browser -> {
+            browser.navigate().refresh();
+            System.out.println("Checking existence of instance with API URL: " + apiUrl);
+            return isInstanceCreated(apiUrl);
+        };
+
+        Graphene.waitGui()
+                .withTimeout(5, TimeUnit.MINUTES)
+                .withMessage("Coupa instance with API URL: '" + apiUrl + "' wasn't created.")
+                .pollingEvery(10, TimeUnit.SECONDS)
+                .until(predicate);
+
+        takeScreenshot(browser, "created-instance-exists", getClass());
+        System.out.println("Coupa instance with API URL: " + apiUrl + " was created");
+    }
+
+    private boolean isInstanceCreated(final String apiUrl) {
+        notEmpty(apiUrl, "apiUrl cannot be empty!");
+
+        try {
+            final JSONObject json = loadJSON();
+            final JSONArray coupaInstances = json.getJSONObject("coupaInstances").getJSONArray("items");
+
+            for (int index = 0; index < coupaInstances.length(); index++) {
+                if (apiUrl.equals(coupaInstances.getJSONObject(index).getJSONObject("coupaInstance").getString("apiUrl"))) {
+                    return true;
+                }
+            }
+        } catch (JSONException e) {
+            fail("Reading JSON structure of Coupa instances endpoint failed.", e);
+        }
+
+        return false;
     }
 }
