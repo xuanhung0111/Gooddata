b23efadf58bc4481d5090658d4a11051d157e4da
vhtien <tien.vo@gooddata.com>
2016-09-27 16:53:31 +0700

    RELATED: QA-5789 Create flow for module user management

---
+++ ui-tests-indigo/ui-tests-user-mgmt/src/test/java/com/gooddata/qa/graphene/flow/UITestsRegistry.java
@@ -0,0 +1,25 @@
+package com.gooddata.qa.graphene.flow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.gooddata.qa.utils.flow.TestsRegistry;
+
+public class UITestsRegistry {
+
+    public static void main(String[] args) throws Throwable {
+        Map<String, Object[]> suites = new HashMap<>();
+
+        suites.put("all", new Object[] {
+            "testng-imap-user-mgmt-general.xml"
+        });
+
+        suites.put("sanity", new Object[] {
+            "testng-imap-user-mgmt-sanity.xml"
+        });
+
+        TestsRegistry.getInstance()
+            .register(args, suites)
+            .toTextFile();
+    }
+}
