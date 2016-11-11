aef885af5fc08bc4e9ee708646d21a093cc7cbee
ldkhoa <khoa.le@gooddata.com>
2016-11-11 17:07:10 +0700

    Merge master branch to develop

--- ui-tests-msf/ui-tests-disc/src/test/java/com/gooddata/qa/graphene/flow/UITestsRegistry.java
+++ ui-tests-msf/ui-tests-disc/src/test/java/com/gooddata/qa/graphene/flow/UITestsRegistry.java
@@ -14,7 +14,7 @@ import com.gooddata.qa.utils.flow.TestsRegistry;
 public class UITestsRegistry {
 
     public static void main(String[] args) throws Throwable {
-        Map<String, Object[]> suites = new HashMap<>();
+        Map<String, Object> suites = new HashMap<>();
 
         suites.put("all", new Object[] {
             OverviewPageTest.class,
