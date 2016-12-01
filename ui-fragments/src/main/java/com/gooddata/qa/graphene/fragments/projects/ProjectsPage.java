9e289ae4d15fcbd11ab69b0300bf1cfe4de62363
thuankhuat <thuan.khuat@gooddata.com>
2016-12-01 15:33:15 +0700

    TEST: CL-9515 Graphene test for CL-8844 User can leave the project which is the only administrator in it

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/projects/ProjectsPage.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/projects/ProjectsPage.java
@@ -1,7 +1,9 @@
 package com.gooddata.qa.graphene.fragments.projects;
 
 import static com.gooddata.qa.graphene.utils.WaitUtils.waitForElementVisible;
+import static org.openqa.selenium.By.className;
 import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.tagName;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -80,8 +82,46 @@ public class ProjectsPage extends AbstractFragment {
                 .getText();
     }
 
+    public ProjectsPage leaveProject(String name) {
+        waitForElementVisible(findProject(name)).findElement(className("leaveProject")).click();
+        getPopupDialog().leaveProject();
+
+        return this;
+    }
+
+    public PopupDialog getPopupDialog() {
+        return Graphene.createPageFragment(PopupDialog.class, waitForElementVisible(className
+                ("t-infoMessageDialog"), browser));
+    }
+
+    private WebElement findProject(String name) {
+        return projects.stream().filter(e -> name.equals(e.findElement(className("projectTitle")).getText()))
+                .findFirst().get();
+    }
+
     private String getProjectIdFrom(WebElement project) {
         String gdcLink = project.getAttribute("gdc:link");
         return gdcLink.substring(gdcLink.lastIndexOf("/") + 1);
+    }
+
+    public class PopupDialog extends AbstractFragment {
+        @FindBy(className = "s-btn-leave")
+        private WebElement leaveButton;
+
+        public void leaveProject() {
+            waitForElementVisible(leaveButton).click();
+        }
+
+        public String getMessage() {
+            // getText() returns value contains child text, so it should be removed from error msg
+            String returnString = waitForElementVisible(tagName("form"), getRoot()).getText();
+
+            return returnString.substring(0, returnString.indexOf(waitForElementVisible(className("bd_controls"),
+                    getRoot()).getText())).trim();
+        }
+
+        public void close() {
+            waitForElementVisible(className("container-close"), getRoot()).click();
+        }
     }
 }
