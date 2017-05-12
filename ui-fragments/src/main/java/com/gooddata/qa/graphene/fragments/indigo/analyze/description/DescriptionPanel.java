e23b72be5ffa5ae8079bcb8a492ec616239bb19a
Anh Nguyen Thi Quynh <ntqanh@lhv.vn>
2017-05-12 16:28:19 +0700

    TRIVIAL: get description in pop-up panel s wrong

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/indigo/analyze/description/DescriptionPanel.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/indigo/analyze/description/DescriptionPanel.java
@@ -1,15 +1,13 @@
 package com.gooddata.qa.graphene.fragments.indigo.analyze.description;
 
 import static com.gooddata.qa.graphene.utils.WaitUtils.waitForElementVisible;
+import static com.gooddata.qa.graphene.utils.WaitUtils.waitForElementPresent;
 
-import org.jboss.arquillian.graphene.Graphene;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import com.gooddata.qa.graphene.fragments.AbstractFragment;
-import com.google.common.base.Predicate;
 
 public class DescriptionPanel extends AbstractFragment {
 
@@ -28,57 +26,54 @@ public class DescriptionPanel extends AbstractFragment {
     @FindBy(className = "s-dataset-name")
     private WebElement dataset;
 
-    public static final By LOCATOR = By.cssSelector(".adi-catalogue-item-details");
+    public static final By LOCATOR = By.className("adi-catalogue-item-details");
+    private static final By CATALOG_BUBBLE_LOADED = By.className("s-catalogue-bubble-loaded");
 
     private static final String NEW_LINE = "\n";
-    private static final String LOADING = "Loading...";
 
     public String getTimeDescription() {
         return getPrefix(true).toString();
     }
 
     public String getAttributeDescription() {
-        StringBuilder builder = getPrefix(false);
-
-        builder.append("Values").append(NEW_LINE);
         waitForDataLoaded();
-        builder.append(waitForElementVisible(value).getText()).append(NEW_LINE);
 
-        return builder.toString();
+        return getPrefix(false).append("Values")
+                .append(NEW_LINE)
+                .append(waitForElementVisible(value).getText())
+                .append(NEW_LINE)
+                .toString();
     }
 
     public String getMetricDescription() {
-        StringBuilder builder = getPrefix(false);
-
-        builder.append("Defined As").append(NEW_LINE);
         waitForDataLoaded();
-        builder.append(waitForElementVisible(value).getText()).append(NEW_LINE);
 
-        return builder.toString();
+        return getPrefix(false).append("Defined As")
+                .append(NEW_LINE)
+                .append(waitForElementVisible(value).getText())
+                .append(NEW_LINE)
+                .toString();
     }
 
     public String getFactDescription() {
-        StringBuilder builder = getPrefix(false);
-
-        builder.append("Dataset").append(NEW_LINE);
-        builder.append(waitForElementVisible(dataset).getText()).append(NEW_LINE);
-
-        return builder.toString();
+        return getPrefix(false).append("Dataset")
+                .append(NEW_LINE)
+                .append(waitForElementVisible(dataset).getText())
+                .append(NEW_LINE)
+                .toString();
     }
 
     private StringBuilder getPrefix(boolean isTime) {
-        StringBuilder builder = new StringBuilder();
-
-        builder.append(waitForElementVisible(title).getText()).append(NEW_LINE);
-        if (isTime) builder.append(waitForElementVisible(titleDescription).getText()).append(NEW_LINE);
-        builder.append("Field Type").append(NEW_LINE);
-        builder.append(waitForElementVisible(fieldType).getText()).append(NEW_LINE);
-
-        return builder;
+        return new StringBuilder().append(waitForElementVisible(title).getText())
+                .append(NEW_LINE)
+                .append(isTime? waitForElementVisible(titleDescription).getText() + NEW_LINE: "")
+                .append("Field Type")
+                .append(NEW_LINE)
+                .append(waitForElementVisible(fieldType).getText())
+                .append(NEW_LINE);
     }
 
     private void waitForDataLoaded() {
-        Predicate<WebDriver> dataLoaded = browser -> !LOADING.equals(value.getText());
-        Graphene.waitGui().until(dataLoaded);
+        waitForElementPresent(CATALOG_BUBBLE_LOADED, browser);
     }
 }
