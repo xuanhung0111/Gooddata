2fdd1767cec779afc99d2071c56c6fd155aac2ad
tuantranthanh <tuan.tran@gooddata.com>
2018-08-29 05:56:18 +0700

    FEATURE: QA-7989 Prepare separate chrome and firefox dockerfile for automated update process

---
+++ Dockerfile_firefox_spec.rb
@@ -0,0 +1,36 @@
+
+require "serverspec"
+
+describe "Dockerfile" do
+  before(:all) do
+
+    set :backend, :docker
+    set :docker_image, ENV['IMAGE_ID']
+  end
+
+  it "installs the right version of Centos" do
+    expect(file('/etc/redhat-release')).to contain("CentOS")
+  end
+
+  packages = ['xorg-x11-server-Xvfb', 'xorg-x11-xinit',
+              'dejavu-sans-fonts', 'dejavu-sans-mono-fonts', 'gtk3-devel',
+              'dejavu-serif-fonts', 'maven-bin']
+
+  it "installs required packages" do
+    packages.each do |p|
+      expect(package(p)).to be_installed
+    end
+  end
+
+  it "installs Firefox" do
+    expect(command('firefox --version').stdout).to contain("Firefox")
+  end
+
+  it "installs Geckodriver" do
+    expect(command('geckodriver --version').stdout).to contain("geckodriver")
+  end
+
+  it "can start Xvfb-run" do
+    expect(command("xvfb-run echo 'test'").stdout).to eq "test\n"
+  end
+end
