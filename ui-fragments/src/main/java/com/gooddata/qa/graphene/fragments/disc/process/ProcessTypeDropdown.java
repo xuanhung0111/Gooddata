eb47ce426fbd460d5739cd09d817a2a7cda03079
tcsang <sang-truong@gooddata.com>
2019-01-03 18:05:09 +0700

    FEATURE QA-9052: Update Graphene cover ticket : MSF-14286 Unsafe and incorrect behaviour of pipeline component registration API

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/disc/process/ProcessTypeDropdown.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/fragments/disc/process/ProcessTypeDropdown.java
@@ -1,8 +1,13 @@
 package com.gooddata.qa.graphene.fragments.disc.process;
 
 import com.gooddata.qa.graphene.fragments.common.AbstractDropDown;
+import org.jboss.arquillian.graphene.Graphene;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.gooddata.qa.graphene.utils.ElementUtils.isElementPresent;
 import static com.gooddata.qa.graphene.utils.ElementUtils.isElementVisible;
 import static com.gooddata.qa.graphene.utils.WaitUtils.waitForElementVisible;
 import static com.gooddata.qa.utils.CssUtils.simplifyText;
@@ -37,6 +42,15 @@ public class ProcessTypeDropdown extends AbstractDropDown {
     }
 
     public ProcessTypeDropdown selectProcessType(String processType) {
+        JavascriptExecutor js = (JavascriptExecutor) browser;
+        By selector = By.cssSelector(".s-" + simplifyText(processType));
+
+        Graphene.waitGui().withTimeout(60, TimeUnit.SECONDS).pollingEvery(1, TimeUnit.SECONDS).until(browser -> {
+            js.executeScript("arguments[0].scrollTop = arguments[1];",
+                    browser.findElement(By.cssSelector(".ember-view.ember-list-view")), 200);
+            return isElementPresent(selector, browser);
+        });
+
         waitForElementVisible(By.cssSelector(".s-" + simplifyText(processType)), browser).click();
         return this;
     }
