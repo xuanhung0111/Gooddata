9ee58e5cb066c1732128cf0c04a43a7e792aa655
Minh Luu <minh.luu@gooddata.com>
2019-04-16 14:06:05 +0700

    BUGFIX: QA-9507 PrintDashboardTest is failed

--- ui-fragments/src/main/java/com/gooddata/qa/graphene/enums/dashboard/DashboardWidgetDirection.java
+++ ui-fragments/src/main/java/com/gooddata/qa/graphene/enums/dashboard/DashboardWidgetDirection.java
@@ -1,6 +1,8 @@
 package com.gooddata.qa.graphene.enums.dashboard;
 
 import com.gooddata.qa.browser.BrowserUtils;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
@@ -16,7 +18,7 @@ public enum DashboardWidgetDirection {
     UP("(top): (\\d+)px", 0),
     MIDDLE("(top): (\\d+)px", 60),
     DOWN("(top): (\\d+)px", 120),
-    NEXT_PAGE("(top): (\\d+)px", 1250);
+    NEXT_PAGE("(top): (\\d+)px", 1195);
 
     private String pattern;
     private int expectedCoordinates;
@@ -32,6 +34,17 @@ public enum DashboardWidgetDirection {
                 .moveByOffset(distance.get("x"), distance.get("y"))
                 .release()
                 .perform();
+    }
+
+    public void moveElementToRightPlaceOutViewPort(WebElement element) {
+        Map<String, Integer> distance = getDistance(element);
+        WebDriver browser = BrowserUtils.getBrowserContext();
+        browser.manage().window().setSize(new Dimension(1680, 1500));
+        new Actions(browser).clickAndHold(element)
+                .moveByOffset(distance.get("x"), distance.get("y"))
+                .release()
+                .perform();
+        browser.manage().window().setSize(new Dimension(1680, 976));
     }
 
     private Map<String, Integer> getDistance(WebElement element) {
