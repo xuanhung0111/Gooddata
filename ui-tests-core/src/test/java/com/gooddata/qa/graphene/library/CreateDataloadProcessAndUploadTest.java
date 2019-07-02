9d2e8938b65ed040be06105a423fdbc5d19903f5
Bui Ba Khanh Trinh <trinh.bui@gooddata.com>
2019-07-02 10:12:45 +0700

    FEATURE:QA-9689 test creating dataload process and upload webdav using gooddata client

---
+++ ui-tests-core/src/test/java/com/gooddata/qa/graphene/library/CreateDataloadProcessAndUploadTest.java
@@ -0,0 +1,53 @@
+package com.gooddata.qa.graphene.library;
+
+import com.gooddata.GoodData;
+import com.gooddata.dataload.processes.DataloadProcess;
+import com.gooddata.dataload.processes.ProcessType;
+import com.gooddata.qa.graphene.AbstractProjectTest;
+import com.gooddata.qa.graphene.fragments.disc.process.DeployProcessForm.PackageFile;
+import com.gooddata.qa.utils.http.RestClient.RestProfile;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Test creating dataload process and upload to webdav by using new instance of gooddata client
+ * instead of the one used to create project
+ */
+public class CreateDataloadProcessAndUploadTest extends AbstractProjectTest {
+
+    @Test(dependsOnGroups = {"createProject"})
+    public void testCreatingDataloadProcess() {
+        final String processName = generateProcessName();
+        RestProfile profile = getProfile(Profile.ADMIN);
+        GoodData goodDataClient = new GoodData(profile.getHost().getHostName(), profile.getUsername(),
+                profile.getPassword(), profile.getHost().getPort());
+
+        DataloadProcess process = goodDataClient.getProcessService().createProcess(getProject(),
+                new DataloadProcess(processName, ProcessType.GRAPH), PackageFile.ADS_TABLE.loadFile());
+        log.info("Created dataload process: " + processName);
+        goodDataClient.getProcessService().removeProcess(process);
+    }
+
+    @Test(dependsOnGroups = {"createProject"})
+    public void testUploadDataTowebdav() throws IOException {
+        final String webdavFolder = generateWebdavFolder();
+        RestProfile profile = getProfile(Profile.ADMIN);
+        GoodData goodDataClient = new GoodData(profile.getHost().getHostName(), profile.getUsername(),
+                profile.getPassword(), profile.getHost().getPort());
+
+        goodDataClient.getDataStoreService().upload(webdavFolder, new FileInputStream(PackageFile.ADS_TABLE.loadFile()));
+        log.info("Created webdavfolder:" + webdavFolder);
+        goodDataClient.getDataStoreService().delete(webdavFolder);
+        log.info("Deleted webdavfolder:" + webdavFolder);
+    }
+
+    private String generateProcessName() {
+        return "Ads_dataload_process_" + generateHashString();
+    }
+
+    private String generateWebdavFolder() {
+        return "att_webdav_test_" + generateHashString();
+    }
+}
