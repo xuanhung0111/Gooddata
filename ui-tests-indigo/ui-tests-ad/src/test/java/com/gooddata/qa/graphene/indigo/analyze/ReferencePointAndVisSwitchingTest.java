0d9df3ab4019513e7394d4408a39b09338a52860
KhoaPham1010 <khoapham10101992@gmail.com>
2019-08-22 14:41:00 +0700

    FEATURE: QA-8498 Graphene tests to cover CL-12966

--- ui-tests-indigo/ui-tests-ad/src/test/java/com/gooddata/qa/graphene/indigo/analyze/ReferencePointAndVisSwitchingTest.java
+++ ui-tests-indigo/ui-tests-ad/src/test/java/com/gooddata/qa/graphene/indigo/analyze/ReferencePointAndVisSwitchingTest.java
@@ -2,33 +2,44 @@ package com.gooddata.qa.graphene.indigo.analyze;
 
 import com.gooddata.qa.fixture.utils.GoodSales.Metrics;
 import com.gooddata.qa.graphene.entity.visualization.CategoryBucket;
+import com.gooddata.qa.graphene.entity.visualization.FilterAttribute;
 import com.gooddata.qa.graphene.entity.visualization.InsightMDConfiguration;
 import com.gooddata.qa.graphene.entity.visualization.MeasureBucket;
 import com.gooddata.qa.graphene.enums.indigo.ReportType;
 import com.gooddata.qa.graphene.enums.project.ProjectFeatureFlags;
 import com.gooddata.qa.graphene.fragments.indigo.analyze.pages.internals.MetricsBucket;
 import com.gooddata.qa.graphene.fragments.indigo.analyze.pages.internals.FiltersBucket;
+import com.gooddata.qa.graphene.fragments.indigo.analyze.reports.PivotTableReport;
 import com.gooddata.qa.graphene.indigo.analyze.common.AbstractAnalyseTest;
 import com.gooddata.qa.utils.http.RestClient;
 import com.gooddata.qa.utils.http.indigo.IndigoRestRequest;
 import com.gooddata.qa.utils.http.project.ProjectRestRequest;
+import org.apache.commons.lang3.tuple.Pair;
 import org.testng.annotations.Test;
 
-import static com.gooddata.qa.graphene.utils.GoodSalesUtils.ATTR_REGION;
+import java.util.List;
+
+import static com.gooddata.qa.graphene.utils.CheckUtils.checkRedBar;
 import static com.gooddata.qa.graphene.utils.GoodSalesUtils.METRIC_AMOUNT;
 import static com.gooddata.qa.graphene.utils.GoodSalesUtils.METRIC_AVG_AMOUNT;
+import static com.gooddata.qa.graphene.utils.GoodSalesUtils.ATTR_PRODUCT;
+import static com.gooddata.qa.graphene.utils.GoodSalesUtils.ATTR_FORECAST_CATEGORY;
 import static com.gooddata.qa.graphene.utils.GoodSalesUtils.METRIC_WON;
+import static com.gooddata.qa.graphene.utils.GoodSalesUtils.ATTR_REGION;
 import static com.gooddata.qa.graphene.utils.GoodSalesUtils.ATTR_DEPARTMENT;
 import static com.gooddata.qa.graphene.utils.GoodSalesUtils.ATTR_STAGE_NAME;
 import static com.gooddata.qa.graphene.utils.GoodSalesUtils.DATE_DATASET_CREATED;
-import static com.gooddata.qa.graphene.utils.GoodSalesUtils.ATTR_FORECAST_CATEGORY;
+
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertEquals;
 
 public class ReferencePointAndVisSwitchingTest extends AbstractAnalyseTest {
 
     private static final String INSIGHT_HAS_SOME_MEASURES_AND_SOME_ATTRIBUTES = "Some measures, attributes";
+    private static final String INSIGHT_HAS_SOME_MEASURES_AND_AN_ATTRIBUTE = "Some measures, an attribute";
     private IndigoRestRequest indigoRestRequest;
     private ProjectRestRequest projectRestRequest;
 
@@ -52,13 +63,20 @@ public class ReferencePointAndVisSwitchingTest extends AbstractAnalyseTest {
 
     @Test(dependsOnGroups = "createProject")
     public void prepareInsights() {
-        createTableInsight(INSIGHT_HAS_SOME_MEASURES_AND_SOME_ATTRIBUTES);
+        createTableInsight(INSIGHT_HAS_SOME_MEASURES_AND_AN_ATTRIBUTE, ReportType.TABLE,
+            asList(METRIC_WON, METRIC_AMOUNT, METRIC_AVG_AMOUNT),
+            singletonList(Pair.of(ATTR_PRODUCT, CategoryBucket.Type.ATTRIBUTE)), singletonList(ATTR_PRODUCT));
+
+        createTableInsight(INSIGHT_HAS_SOME_MEASURES_AND_SOME_ATTRIBUTES, ReportType.TABLE,
+            asList(METRIC_AMOUNT, METRIC_AVG_AMOUNT), asList(Pair.of(ATTR_DEPARTMENT, CategoryBucket.Type.ATTRIBUTE),
+                Pair.of(ATTR_FORECAST_CATEGORY, CategoryBucket.Type.ATTRIBUTE),
+                Pair.of(ATTR_REGION, CategoryBucket.Type.ATTRIBUTE)),
+            asList(ATTR_DEPARTMENT, ATTR_FORECAST_CATEGORY, ATTR_REGION));
     }
 
     @Test(dependsOnMethods = {"prepareInsights"})
     public void switchToChartDoesNotAllowCombination() {
         initAnalysePage().openInsight(INSIGHT_HAS_SOME_MEASURES_AND_SOME_ATTRIBUTES)
-                .addFilter(ATTR_DEPARTMENT).addFilter(ATTR_FORECAST_CATEGORY).addFilter(ATTR_REGION)
                 .changeReportType(ReportType.PIE_CHART).waitForReportComputing();
 
         assertEquals(analysisPage.getMetricsBucket().getItemNames(), singletonList(METRIC_AMOUNT));
@@ -70,7 +88,6 @@ public class ReferencePointAndVisSwitchingTest extends AbstractAnalyseTest {
     @Test(dependsOnMethods = {"prepareInsights"})
     public void switchToChartAllowCombination() {
         initAnalysePage().openInsight(INSIGHT_HAS_SOME_MEASURES_AND_SOME_ATTRIBUTES)
-            .addFilter(ATTR_DEPARTMENT).addFilter(ATTR_FORECAST_CATEGORY).addFilter(ATTR_REGION)
             .changeReportType(ReportType.PIE_CHART)
             .changeReportType(ReportType.COLUMN_CHART).waitForReportComputing();
 
@@ -118,7 +135,6 @@ public class ReferencePointAndVisSwitchingTest extends AbstractAnalyseTest {
     @Test(dependsOnMethods = {"prepareInsights"})
     public void changeValuesOnFiltersToCreateANewReferencePoint() {
         initAnalysePage().openInsight(INSIGHT_HAS_SOME_MEASURES_AND_SOME_ATTRIBUTES)
-                .addFilter(ATTR_DEPARTMENT).addFilter(ATTR_FORECAST_CATEGORY).addFilter(ATTR_REGION)
                 .changeReportType(ReportType.COLUMN_CHART)
                 .getFilterBuckets().configAttributeFilter(ATTR_DEPARTMENT, "Inside Sales");
         analysisPage.changeReportType(ReportType.TABLE).waitForReportComputing();
@@ -167,18 +183,69 @@ public class ReferencePointAndVisSwitchingTest extends AbstractAnalyseTest {
                 asList(ATTR_DEPARTMENT, ATTR_FORECAST_CATEGORY, ATTR_REGION));
     }
 
-    private void createTableInsight(String title) {
-        indigoRestRequest.createInsight(
-                new InsightMDConfiguration(title, ReportType.TABLE)
-                        .setMeasureBucket(
-                                asList(MeasureBucket.createSimpleMeasureBucket(getMetricByTitle(METRIC_AMOUNT)),
-                                        MeasureBucket.createSimpleMeasureBucket(getMetricByTitle(METRIC_AVG_AMOUNT))))
-                        .setCategoryBucket(asList(
-                                CategoryBucket.createCategoryBucket(getAttributeByTitle(ATTR_DEPARTMENT),
-                                        CategoryBucket.Type.ATTRIBUTE),
-                                CategoryBucket.createCategoryBucket(getAttributeByTitle(ATTR_FORECAST_CATEGORY),
-                                        CategoryBucket.Type.ATTRIBUTE),
-                                CategoryBucket.createCategoryBucket(getAttributeByTitle(ATTR_REGION),
-                                        CategoryBucket.Type.ATTRIBUTE))));
+    @Test(dependsOnMethods = {"prepareInsights"})
+    public void switchFromPieOrDonutToBulletChart() {
+        //https://jira.intgdc.com/browse/CL-12966
+        initAnalysePage().openInsight(INSIGHT_HAS_SOME_MEASURES_AND_AN_ATTRIBUTE).waitForReportComputing()
+            .getPivotTableReport().sortBaseOnHeader(METRIC_AMOUNT);
+
+        analysisPage.waitForReportComputing().changeReportType(ReportType.BUBBLE_CHART).waitForReportComputing();
+        checkRedBar(browser);
+
+        analysisPage.changeReportType(ReportType.PIE_CHART).waitForReportComputing();
+        checkRedBar(browser);
+
+        analysisPage.changeReportType(ReportType.DONUT_CHART).waitForReportComputing();
+        checkRedBar(browser);
+    }
+
+    @Test(dependsOnMethods = {"prepareInsights"})
+    public void switchFromScatterOrBubbleToBulletChart() {
+        //https://jira.intgdc.com/browse/CL-12941
+        initAnalysePage().openInsight(INSIGHT_HAS_SOME_MEASURES_AND_AN_ATTRIBUTE).waitForReportComputing()
+            .getPivotTableReport().sortBaseOnHeader(METRIC_WON);
+
+        analysisPage.waitForReportComputing().changeReportType(ReportType.BUBBLE_CHART).waitForReportComputing();
+        checkRedBar(browser);
+
+        analysisPage.changeReportType(ReportType.SCATTER_PLOT).waitForReportComputing();
+        checkRedBar(browser);
+
+        analysisPage.changeReportType(ReportType.BUBBLE_CHART).waitForReportComputing();
+        checkRedBar(browser);
+    }
+
+    @Test(dependsOnMethods = {"prepareInsights"})
+    public void checkTableConfigurationIsKeptAfterSwitchingChart() {
+        //https://jira.intgdc.com/browse/CL-12904
+        PivotTableReport pivotTableReport = initAnalysePage().openInsight(INSIGHT_HAS_SOME_MEASURES_AND_AN_ATTRIBUTE)
+            .waitForReportComputing().getPivotTableReport().sortBaseOnHeader(METRIC_AMOUNT);
+        analysisPage.waitForReportComputing();
+
+        assertFalse(pivotTableReport.isRowHeaderSortedUp(METRIC_AMOUNT));
+
+        analysisPage.changeReportType(ReportType.BUBBLE_CHART).waitForReportComputing();
+        analysisPage.openConfigurationPanelBucket().expandConfigurationPanel().getItemConfiguration("Legend")
+            .switchOff();
+
+        analysisPage.changeReportType(ReportType.TABLE).waitForReportComputing();
+        checkRedBar(browser);
+        assertFalse(pivotTableReport.isRowHeaderSortedUp(METRIC_AMOUNT));
+    }
+
+    private String createTableInsight(String insightTitle, ReportType reportType, List<String> metricsTitle,
+                                 List<Pair<String, CategoryBucket.Type>> attributeConfigurations, List<String> filterAttributes) {
+        return indigoRestRequest.createInsight(
+            new InsightMDConfiguration(insightTitle, reportType)
+                .setMeasureBucket(metricsTitle.stream()
+                    .map(metric -> MeasureBucket.createSimpleMeasureBucket(getMetricByTitle(metric)))
+                    .collect(toList()))
+                .setCategoryBucket(attributeConfigurations.stream()
+                    .map(attribute -> CategoryBucket.createCategoryBucket(
+                        getAttributeByTitle(attribute.getKey()), attribute.getValue()))
+                    .collect(toList()))
+                .setFilter(filterAttributes.stream()
+                    .map(filter -> FilterAttribute.createFilter(getAttributeByTitle(filter)))
+                    .collect(toList())));
     }
 }
