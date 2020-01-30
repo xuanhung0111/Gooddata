7d351fde7f7b4cea95b841ea8401c08d832342f6
tuantranthanh <tuan.tran@gooddata.com>
2020-01-30 12:07:46 +0700

    TRIVIAL: fix missing space in integration bash script

--- integration-mvn-test.sh
+++ integration-mvn-test.sh
@@ -2,4 +2,4 @@
 
 xvfb-run -a -s "-screen 0 1680x1050x24 -noreset" mvn integration-test -Duser.home=/tmp -DscreenResolution=1680x1050 \
     -Dlanguage=en-US -Dtest.execution.env=local-chrome -Dtimeout=90 -Dvideo.recording=true -DtestIdentification=CI#$BUILD_NUMBER#$JOB_NAME#$BUILD_ID \
-    -DchromeDriverBinaryPath=/bin/chromedriver -DchromeDriverArguments=no-sandbox-Pselenium -am "$@"
+    -DchromeDriverBinaryPath=/bin/chromedriver -DchromeDriverArguments=no-sandbox -Pselenium -am "$@"
