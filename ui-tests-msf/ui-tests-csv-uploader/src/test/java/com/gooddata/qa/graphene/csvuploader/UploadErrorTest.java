223d68d6fc261fab9f35d23905e5268dd3364d89
tuantranthanh <tuan.tran@gooddata.com>
2020-04-06 14:09:18 +0700

    BUGFIX: MSF-17273 adapt the log msg of csv uploader frontend part

--- ui-tests-msf/ui-tests-csv-uploader/src/test/java/com/gooddata/qa/graphene/csvuploader/UploadErrorTest.java
+++ ui-tests-msf/ui-tests-csv-uploader/src/test/java/com/gooddata/qa/graphene/csvuploader/UploadErrorTest.java
@@ -31,8 +31,10 @@ import com.gooddata.qa.graphene.fragments.csvuploader.FileUploadDialog;
 
 public class UploadErrorTest extends AbstractCsvUploaderTest {
 
-    private static final String TOO_LONG_COLUMN_OR_TOO_MANY_COLUMNS_ERROR =
-            "Row %d contains a value that exceeds the limit of 255 characters, or has more than the limit of 250 columns.";
+    private static final String TOO_MANY_COLUMNS_ERROR =
+            "Row %s contains a value that exceeds the limit of characters, or has more than the limit of %s columns.";
+    private static final String TOO_LONG_TEXT_OF_COLUMN =
+            "Row %s contains a value that exceeds the limit of %s characters, or has more than the limit of columns.";
     private static final String ROW_CONTAINS_MORE_COLUMNS_THAN_THE_HEADER_ROW =
             "Row %s contains more columns than the header row.";
     private static final String DATA_WITHOUT_FACT =
@@ -64,8 +66,8 @@ public class UploadErrorTest extends AbstractCsvUploaderTest {
                 {withoutFact, asList(DATA_WITHOUT_FACT)},
                 {invalidDelimiter, asList(DATA_WITHOUT_FACT)},
                 {BAD_STRUCTURE_FILE, asList(format(ROW_CONTAINS_MORE_COLUMNS_THAN_THE_HEADER_ROW, 2))},
-                {tooManyColumns, asList(format(TOO_LONG_COLUMN_OR_TOO_MANY_COLUMNS_ERROR, 1))},
-                {tooLongField, asList(format(TOO_LONG_COLUMN_OR_TOO_MANY_COLUMNS_ERROR, 2))},
+                {tooManyColumns, asList(format(TOO_MANY_COLUMNS_ERROR, 1))},
+                {tooLongField, asList(format(TOO_LONG_TEXT_OF_COLUMN, 2, 255))},
                 {crazyData, asList("There are 5 rows containing less columns than the header row: 44-48.",
                         "There are 5 rows without at least one numerical value: 44-48. "
                                 + "Each row must contain numerical data for analysis.")}
