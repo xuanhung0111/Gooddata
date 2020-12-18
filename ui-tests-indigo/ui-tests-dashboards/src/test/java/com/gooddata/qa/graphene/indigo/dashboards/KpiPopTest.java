4bc9edf688704259670c95a215d1402498be091b
nguyen mai <nhmai0826@gmail.com>
2020-12-18 10:37:27 +0700

    FEARURE:QA-12306 Graphene test to cover RAIL-2692

--- ui-tests-indigo/ui-tests-dashboards/src/test/java/com/gooddata/qa/graphene/indigo/dashboards/KpiPopTest.java
+++ ui-tests-indigo/ui-tests-dashboards/src/test/java/com/gooddata/qa/graphene/indigo/dashboards/KpiPopTest.java
@@ -156,4 +156,23 @@ public class KpiPopTest extends AbstractDashboardTest {
         indigoDashboardsPage.switchToEditMode().getLastWidget(Kpi.class).delete();
         indigoDashboardsPage.saveEditModeWithWidgets();
     }
+
+    @Test(dependsOnGroups = {"createProject"}, groups = {"desktop"})
+    public void checkKpiPopSectionWhenFilteringByFloatingRange() {
+        Kpi kpi = initIndigoDashboardsPageWithWidgets()
+                .switchToEditMode()
+                .addKpi(new KpiConfiguration.Builder()
+                        .metric(METRIC_AMOUNT)
+                        .dataSet(DATE_DATASET_CREATED)
+                        .comparison(ComparisonType.LAST_YEAR.toString())
+                        .build())
+                .selectLastWidget(Kpi.class);
+        indigoDashboardsPage.openExtendedDateFilterPanel().selectFloatingRange(ExtendedDateFilterPanel.DateGranularity.YEARS,"9 years ago","2 years ago").apply();
+        takeScreenshot(browser, "addFloatingRange", getClass());
+        indigoDashboardsPage.saveEditModeWithWidgets().waitForWidgetsLoading();
+        takeScreenshot(browser, "checkSaveKDWithFloatingDate", getClass());
+        Kpi lastKpi = indigoDashboardsPage.getLastWidget(Kpi.class);
+        assertTrue(lastKpi.hasPopSection(), "Newly added KPI should have pop section");
+        assertEquals(indigoDashboardsPage.getDateFilter().getSelection(), "From 9 to 2 years ago");
+    }
 }
