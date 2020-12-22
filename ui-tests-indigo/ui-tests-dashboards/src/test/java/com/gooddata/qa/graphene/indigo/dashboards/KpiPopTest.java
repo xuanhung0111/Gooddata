51a954ebd4d35d4b6def8ac6e8564445efe33d8c
nguyen mai <nhmai0826@gmail.com>
2020-12-22 10:36:23 +0700

    FEARURE:QA-12306 Graphene test to cover RAIL-2692

--- ui-tests-indigo/ui-tests-dashboards/src/test/java/com/gooddata/qa/graphene/indigo/dashboards/KpiPopTest.java
+++ ui-tests-indigo/ui-tests-dashboards/src/test/java/com/gooddata/qa/graphene/indigo/dashboards/KpiPopTest.java
@@ -14,6 +14,7 @@ import static org.testng.Assert.assertTrue;
 import java.util.List;
 
 import com.gooddata.qa.graphene.enums.DateRange;
+import com.gooddata.qa.graphene.fragments.indigo.dashboards.ExtendedDateFilterPanel;
 import com.gooddata.qa.utils.http.RestClient;
 import com.gooddata.qa.utils.http.indigo.IndigoRestRequest;
 import org.testng.annotations.DataProvider;
